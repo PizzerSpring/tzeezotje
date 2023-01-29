3458520642cc6599005ffbc0a53a973c97d55a8f
PizzerSpring <vladi-slav96@mail.ru>
2023-01-29 11:19:23 +0700

    slider

---
+++ script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      disabledClass: 'disabled_swiper_button'
+    },
+  });
+    
